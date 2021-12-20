86425a042dd2113122c21d257d587d17791405e2
Nicola Narducci <nicola.narducci@outlook.com>
2021-12-20 22:39:53 +0100

    Added Eplosions and Splits

--- AdventOfCode2021/Day18.cs
+++ AdventOfCode2021/Day18.cs
@@ -12,72 +12,249 @@ public class Day18
         return 0;
     }
 
-    
+
     public static long Sol2()
     {
         return 0;
     }
-   
+
 }
 
-public class SFMTuple{
+public class SnailMath
+{
 
-    public SFMTuple Parent {get; private set;}
-    public SFMTuple Left {get; private set;}
-    public SFMTuple Right {get; private set;}
+    public Guid Id { get; }
+    public SnailMath? Parent { get; set; }
+    public SnailMath? Left { get; set; }
+    public SnailMath? Right { get; set; }
 
-    public int Value {get; private set;}
+    public int Value { get; set; }
 
-    public SFMTuple(string input, SFMTuple parent){
+    public int Depth => Parent?.Depth + 1 ?? 0;
+
+    public bool ToSplit => Value > 10;
+
+    public bool ToExplode => Depth > 4;
+
+
+    public SnailMath(string input) : this(input, null)
+    {
+
+    }
+
+    public SnailMath(int value, SnailMath parent)
+    {
+        Id = Guid.NewGuid();
+        Value = value;
         Parent = parent;
-        if(!input.Contains("[")){
-            var i = input.Split(",").Select(int.Parse).ToArray();
-            Left = new SFMTuple(i[0], this);
-            Right = new SFMTuple(i[1], this);
-        }else{
-            
+    }
+
+
+    public SnailMath(string input, SnailMath? parent)
+    {
+        Id = Guid.NewGuid();
+        Parent = parent;
+        if (input.Contains(','))
+        {
+            var index = FindComma(input);
+            Left = new SnailMath(input[1..index], this);
+            Right = new SnailMath(input[(index + 1)..^1], this);
+        }
+        else
+        {
+            Left = null;
+            Right = null;
+            Value = int.Parse(input);
         }
     }
 
-    public SFMTuple(int value, SFMTuple parent){
-        Parent = parent;
-        Value = value;
-        Left = null;
-        Right = null;
+    public SnailMath(SnailMath left, SnailMath right)
+    {
+        Id = Guid.NewGuid();
+        Parent = null;
+        Left = left;
+        left.Parent = this;
+        Right = right;
+        right.Parent = this;
     }
 
-    public void ParseInput(string input){
-
-    }
-
-    public static void ParseLine(string line)
+    public static int FindComma(string line)
     {
         var depth = 0;
-        var dictionary = new Dictionary<int,string>();
+        var dictionary = new Dictionary<int, string>();
         for (int i = 0; i < line.Length; i++)
         {
             var c = line[i];
-            if(c == '['){
+            if (c == '[')
+            {
                 depth++;
             }
-            else if (c == ']'){
-                var t = new SFMTuple(dictionary[depth], null);
+            else if (c == ']')
+            {
                 dictionary.Remove(depth);
                 depth--;
             }
-            else{
-                dictionary[depth] = dictionary.GetValueOrDefault(depth, "") + c;
+            else if (c == ',' && depth == 1)
+            {
+                return i;
             }
+        }
+        return -1;
+    }
+
+    public long GetMagnitude()
+    {
+        if (Left == null && Right == null)
+        {
+            return Value;
+        }
+        else
+        {
+            return 3 * Left.GetMagnitude() + 2 * Right.GetMagnitude();
+        }
+    }
+}
+
+public class SnailTree
+{
+    public SnailMath Root { get; set; }
+
+    public List<SnailMath> OrderedSnailMath { get; private set; }
+    public Dictionary<int, Guid> IdByPosition { get; private set; }
+    public Dictionary<Guid, int> PositionById { get; private set; }
+
+    public Dictionary<Guid, SnailMath> NodesById { get; private set; }
+
+    public SnailTree(string input)
+    {
+        Root = new SnailMath(input);
+        PopulateDictionaries();
+    }
+
+    public void PopulateDictionaries()
+    {
+        OrderedSnailMath = PreorderTraversal(Root);
+
+        IdByPosition = OrderedSnailMath.Select((x, idx) => new { value = x.Id, index = idx }).ToList().ToDictionary(x => x.index, x => x.value);
+        PositionById = IdByPosition.ToDictionary(x => x.Value, x => x.Key);
+        NodesById = OrderedSnailMath.ToDictionary(x => x.Id, x => x);
+    }
+
+    public static List<SnailMath> PreorderTraversal(SnailMath node)
+    {
+        var list = new List<SnailMath>();
+        if (node.Left != null)
+        {
+            list.AddRange(PreorderTraversal(node.Left));
+        }
+        if (node.Right != null)
+        {
+            list.AddRange(PreorderTraversal(node.Right));
+        }
+        if (node.Right == null && node.Left == null)
+        {
+            list.Add(node);
+        }
+        return list;
+    }
+
+    public void ExplodeLeft(SnailMath snailMath)
+    {
+        var position = PositionById[snailMath.Id];
+        if (position > 0)
+        {
+            var previous = NodesById[IdByPosition[position - 1]];
+            previous.Value += snailMath.Value;
+        }
+    }
+    public void ExplodeRight(SnailMath snailMath)
+    {
+        var position = PositionById[snailMath.Id];
+        if (position < PositionById.Count - 1)
+        {
+            var successor = NodesById[IdByPosition[position + 1]];
+            successor.Value += snailMath.Value;
         }
     }
 
-    
-
-    public void ExplodeOne(){
-
+    public void Explode(SnailMath snailMath)
+    {
+        ExplodeLeft(snailMath!.Left!);
+        ExplodeRight(snailMath!.Right!);
+        if (snailMath.Parent != null)
+        {
+            if (snailMath.Parent.Left != null && snailMath.Parent.Left.Id == snailMath.Id)
+            {
+                snailMath.Parent.Left = new SnailMath(0, snailMath.Parent);
+            }
+            else if (snailMath.Parent.Right != null && snailMath.Parent.Right.Id == snailMath.Id)
+            {
+                snailMath.Parent.Right = new SnailMath(0, snailMath.Parent);
+            }
+        }
+        PopulateDictionaries();
     }
 
-    public void SplitOne()
+    public void Split(SnailMath snailMath)
     {
+        var left = new SnailMath(snailMath.Value / 2, snailMath);
+        var right = new SnailMath(snailMath.Value - left.Value, snailMath);
+        snailMath.Value = 0;
+        snailMath.Left = left;
+        snailMath.Right = right;
+        PopulateDictionaries();
+    }
+
+    public SnailMath? FindNextOperation()
+    {
+        foreach (var snailMath in OrderedSnailMath)
+        {
+            if (snailMath.ToSplit)
+            {
+                return snailMath;
+            }
+            else if (snailMath.ToExplode)
+            {
+                return snailMath.Parent;
+            }
+        }
+        return null;
+    }
+
+    public bool ExecuteNextOperation()
+    {
+        var nextOperation = FindNextOperation();
+        if (nextOperation != null)
+        {
+            if (nextOperation.ToSplit)
+            {
+                Split(nextOperation);
+                return true;
+            }
+            else
+            {
+
+                Explode(nextOperation);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Reduce()
+    {
+        bool reduced = false;
+        do
+        {
+            reduced = ExecuteNextOperation();
+        } while (reduced);
+    }
+
+    public void Add(string input)
+    {
+        var newNode = new SnailMath(input);
+        Root = new SnailMath(Root, newNode);
+        PopulateDictionaries();
+        Reduce();
     }
 }
--- AdventOfCode2021Tests/Day18Tests.cs
+++ AdventOfCode2021Tests/Day18Tests.cs
@@ -1,30 +1,220 @@
 using System.Drawing;
+using System.Linq;
 using Xunit;
 
 namespace AdventOfCode2021Tests
 {
     public class Day18Tests
     {
-
-        [InlineData("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]")]
-        [InlineData("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]")]
-        [InlineData("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]")]
-        [InlineData("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")]
-        [InlineData("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")]
-        public void ExplodeOneTest(string input, string expected){
-            var tupla = new SFMTuple(input, null);
-            tupla.ExplodeOne();
-            Assert.Equal(expected, tupla.ToString());
+        [Theory]
+        [InlineData("[1,2]", 2)]
+        [InlineData("[[1,2],3]", 6)]
+        [InlineData("[9,[8,7]]", 2)]
+        [InlineData("[[1,9],[8,5]]", 6)]
+        [InlineData("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]", 30)]
+        [InlineData("[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]", 22)]
+        [InlineData("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]", 30)]
+        [InlineData("[[[[1,3],[5,3]],[[1,3],[8,7]]][[[4,9],[6,9]],[[8,2],[7,3]]]]", -1)]
+        public void FindCommaTest(string input, int expectedIndex)
+        {
+            var index = SnailMath.FindComma(input);
+            Assert.Equal(expectedIndex, index);
         }
 
-        [InlineData("10", "[5,5]")]
-        [InlineData("11", "[5,6]")]
-        [InlineData("12", "[6,6]")]
-        public void SplitOneTest(string input, string expected){
-            var tupla = new SFMTuple(input, null);
-            tupla.SplitOne();
-            Assert.Equal(expected, tupla.ToString());
+        [Fact]
+        public void SFMTupleSimpleConstrTest()
+        {
+            var t = new SnailMath("[1,2]");
+            Assert.Equal(1, t.Left!.Value);
+            Assert.Equal(2, t.Right!.Value);
         }
 
+
+        [Fact]
+        public void SFMTupleSimpleConstrTest2()
+        {
+            var t = new SnailMath("[[1,2],3]");
+            Assert.Equal(1, t.Left!.Left!.Value);
+            Assert.Equal(2, t.Left!.Right!.Value);
+            Assert.Equal(3, t.Right!.Value);
+        }
+
+        [Fact]
+        public void SFMTupleSimpleConstrTest3()
+        {
+            var t = new SnailMath("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]");
+            Assert.Equal(1, t.Left!.Left!.Left!.Left!.Value);
+            Assert.Equal(3, t.Left!.Left!.Left!.Right!.Value);
+            Assert.Equal(5, t.Left!.Left!.Right!.Left!.Value);
+            Assert.Equal(3, t.Left!.Left!.Right!.Right!.Value);
+            Assert.Equal(3, t.Right!.Right!.Right!.Right!.Value);
+        }
+
+        [Fact]
+        public void SnailTreeExplodeTest()
+        {
+            var snailTree = new SnailTree("[[[[[9,8],1],2],3],4]");
+            snailTree.Explode(snailTree.Root.Left.Left.Left.Left);
+            Assert.Equal(0, snailTree.Root.Left.Left.Left.Left.Value);
+            Assert.Equal(9, snailTree.Root.Left.Left.Left.Right.Value);
+        }
+
+        [Fact]
+        public void SnailTreeExplodeTest2()
+        {
+            var snailTree = new SnailTree("[7,[6,[5,[4,[3,2]]]]]");
+            snailTree.Explode(snailTree.Root.Right.Right.Right.Right);
+            Assert.Equal(0, snailTree.Root.Right.Right.Right.Right.Value);
+            Assert.Equal(7, snailTree.Root.Right.Right.Right.Left.Value);
+        }
+
+        [Fact]
+        public void SnailTreeExplodeTest3()
+        {
+            var snailTree = new SnailTree("[[6,[5,[4,[3,2]]]],1]");
+            snailTree.Explode(snailTree.Root.Left.Right.Right.Right);
+            Assert.Equal(0, snailTree.Root.Left.Right.Right.Right.Value);
+            Assert.Equal(7, snailTree.Root.Left.Right.Right.Left.Value);
+            Assert.Equal(3, snailTree.Root.Right.Value);
+        }
+
+
+        [Fact]
+        public void SnailTreeExplodeTest4()
+        {
+            var snailTree = new SnailTree("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]");
+            snailTree.Explode(snailTree.Root.Left.Right.Right.Right);
+            Assert.Equal(0, snailTree.Root.Left.Right.Right.Right.Value);
+            Assert.Equal(8, snailTree.Root.Left.Right.Right.Left.Value);
+            Assert.Equal(9, snailTree.Root.Right.Left.Value);
+        }
+
+        [Fact]
+        public void SnailTreeExplodeTest5()
+        {
+            var snailTree = new SnailTree("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]");
+            snailTree.Explode(snailTree.Root.Right.Right.Right.Right);
+            Assert.Equal(0, snailTree.Root.Right.Right.Right.Right.Value);
+            Assert.Equal(8, snailTree.Root.Left.Right.Right.Left.Value);
+            Assert.Equal(9, snailTree.Root.Right.Left.Value);
+        }
+
+        [Fact]
+        public void SnailTreeSplitTest()
+        {
+            var snailTree = new SnailTree("[[[[0,7],4],[15,[0,13]]],[1,1]]");
+            snailTree.Split(snailTree.Root.Left.Right.Left);
+            //[[[[0,7],4],[[7,8],[0,13]]],[1,1]]
+            Assert.Equal(7, snailTree.Root.Left.Right.Left.Left.Value);
+            Assert.Equal(8, snailTree.Root.Left.Right.Left.Right.Value);
+        }
+
+        [Fact]
+        public void SnailMathToSplitTest()
+        {
+            var snailNode = new SnailMath("[[[[0,7],4],[15,[0,13]]],[1,1]]");
+            Assert.Equal(true, snailNode.Left.Right.Left.ToSplit);
+        }
+
+        [Fact]
+        public void SnailMathDepthTest()
+        {
+            var snailMath = new SnailMath("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]");
+            Assert.Equal(4, snailMath.Right.Right.Right.Right.Depth);
+            Assert.Equal(true, snailMath.Right.Right.Right.Right.Right.ToExplode);
+        }
+
+        [Fact]
+        public void SnailMathToExplodePropTest()
+        {
+            var snailMath = new SnailMath("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]");
+        }
+
+        [Fact]
+        public void ReduceTest()
+        {
+            var snailTree = new SnailTree("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]");
+            snailTree.Reduce();
+            var reduced = new SnailTree("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+        }
+
+        [Fact]
+        public void AddTest()
+        {
+            var snailTree = new SnailTree("[1,1]");
+            snailTree.Add("[2,2]");
+            snailTree.Add("[3,3]");
+            snailTree.Add("[4,4]");
+            var reduced = new SnailTree("[[[[1,1],[2,2]],[3,3]],[4,4]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+        }
+
+        [Fact]
+        public void AddTest2()
+        {
+            var snailTree = new SnailTree("[1,1]");
+            snailTree.Add("[2,2]");
+            snailTree.Add("[3,3]");
+            snailTree.Add("[4,4]");
+            snailTree.Add("[5,5]");
+            snailTree.Add("[6,6]");
+            var reduced = new SnailTree("[[[[5,0],[7,4]],[5,5]],[6,6]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+        }
+
+        [Fact]
+        public void AddTest3()
+        {
+            var snailTree = new SnailTree("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]");
+            snailTree.Add("[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]");
+            var reduced = new SnailTree("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+
+            snailTree.Add("[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]");
+            reduced = new SnailTree("[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+
+            snailTree.Add("[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]");
+            reduced = new SnailTree("[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+
+            snailTree.Add("[7,[5,[[3,8],[1,4]]]]");
+            reduced = new SnailTree("[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+
+            snailTree.Add("[[2,[2,2]],[8,[8,1]]]");
+            reduced = new SnailTree("[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+
+            snailTree.Add("[2,9]");
+            reduced = new SnailTree("[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+
+            snailTree.Add("[1,[[[9,3],9],[[9,0],[0,7]]]]");
+            reduced = new SnailTree("[[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+
+            snailTree.Add("[[[5,[7,4]],7],1]");
+            reduced = new SnailTree("[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+
+            snailTree.Add("[[[[4,2],2],6],[8,7]]");
+            reduced = new SnailTree("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]");
+            Assert.Equal(reduced.OrderedSnailMath.Select(x => x.Value), snailTree.OrderedSnailMath.Select(x => x.Value));
+        }
+
+        [Theory]
+        [InlineData("[[1,2],[[3,4],5]]", 143)]
+        [InlineData("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384)]
+        [InlineData("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445)]
+        [InlineData("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791)]
+        [InlineData("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137)]
+        [InlineData("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488)]
+        public void MagnitudeTest(string input, int expected)
+        {
+            var snailMath = new SnailMath(input);
+            Assert.Equal(expected, snailMath.GetMagnitude());
+        }
     }
 }
